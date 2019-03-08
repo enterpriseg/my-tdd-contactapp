4f8de67e6cf1aaf06cf28e4bae28fdc77feb5ffa
Daniel Lai <dlai813@gmail.com>
2019-03-08 00:32:19 -0800

    [Init] enzyme test setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import App from "./App";
+
+describe("App", () => {
+	it("renders a main div", () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.find("div#main")).toHaveLength(1);
+	});
 });
