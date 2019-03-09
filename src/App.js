1919b0de70e94aa0d716fac51120605831275fb0
Daniel Lai <dlai813@gmail.com>
2019-03-09 00:00:36 -0800

    [In progress] establish basic structre for App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
 
 import "./App.css";
+import AddPage from "./AddPage";
 
 class App extends Component {
+	state = {
+		people: [{ name: `Person's name` }]
+	};
+
+	addPerson = person => {
+		this.setState({ people: [...this.state.people, person] });
+	};
 	render() {
 		return (
 			<div id="main">
 				<button id="addPerson" />
+				<AddPage addPerson={this.addPerson} />
 			</div>
 		);
 	}
--- src/App.test.js
+++ src/App.test.js
@@ -4,13 +4,33 @@ import { shallow } from "enzyme";
 import App from "./App";
 
 describe("App", () => {
+	let wrapper;
+	beforeEach(() => {
+		wrapper = shallow(<App />);
+	});
 	it("renders a main div", () => {
-		const wrapper = shallow(<App />);
 		expect(wrapper.find("div#main")).toHaveLength(1);
 	});
 
 	it("renders a Add Person button", () => {
-		const wrapper = shallow(<App />);
 		expect(wrapper.find("button#addPerson")).toHaveLength(1);
+	});
+
+	it("allows me to add person to the default", () => {
+		const defaultPeople = [{ name: `Person's name` }];
+		const newPerson = { name: "Daniel" };
+
+		expect(wrapper.state().people).toEqual(defaultPeople);
+
+		// exercise
+		wrapper.instance().addPerson(newPerson);
+		// assertion
+		expect(wrapper.state().people).toEqual([...defaultPeople, newPerson]);
+	});
+
+	it("gives AddPage addPerson", () => {
+		const addPage = wrapper.find("AddPage");
+
+		expect(addPage.props().addPerson).toEqual(wrapper.instance().addPerson);
 	});
 });
