84b4b20e4724a95d62c017284736c742aeb9144c
Daniel Lai <dlai813@gmail.com>
2019-03-08 23:19:38 -0800

    [Refactored] routing test

--- src/Router.js
+++ src/Router.js
@@ -4,12 +4,16 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import NotFound from "./NotFound";
 import ListPage from "./ListPage";
+import AddPage from "./AddPage";
+import EditPage from "./EditPage";
 
 const Router = () => {
 	return (
 		<BrowserRouter>
 			<Switch>
 				<Route exact path="/" component={ListPage} />
+				<Route path="/add/:personId" component={AddPage} />
+				<Route path="/edit/:personId" component={EditPage} />
 				<Route component={NotFound} />
 			</Switch>
 		</BrowserRouter>
--- src/Router.test.js
+++ src/Router.test.js
@@ -5,25 +5,56 @@ import { MemoryRouter } from "react-router";
 import Router from "./Router";
 import ListPage from "./ListPage";
 import NotFound from "./NotFound";
+import AddPage from "./AddPage";
+import EditPage from "./EditPage";
 
 describe("Router", () => {
+	let wrapper;
+
 	it("redirects to 404 for invalid path", () => {
-		const wrapper = mount(
+		wrapper = mount(
 			<MemoryRouter initialEntries={["/random"]}>
 				<Router />
 			</MemoryRouter>
 		);
 		expect(wrapper.find(ListPage)).toHaveLength(0);
+		expect(wrapper.find(AddPage)).toHaveLength(0);
+		expect(wrapper.find(EditPage)).toHaveLength(0);
 		expect(wrapper.find(NotFound)).toHaveLength(1);
 	});
 
-	it("redirects to ListPage as default home page", () => {
-		const wrapper = mount(
+	it("redirects to correct pages according to valid path", () => {
+		// ListPage(Home)
+		wrapper = mount(
 			<MemoryRouter initialEntries={["/"]}>
 				<Router />
 			</MemoryRouter>
 		);
 		expect(wrapper.find(ListPage)).toHaveLength(1);
+		expect(wrapper.find(AddPage)).toHaveLength(0);
+		expect(wrapper.find(EditPage)).toHaveLength(0);
+		expect(wrapper.find(NotFound)).toHaveLength(0);
+
+		// AddPage
+		wrapper = mount(
+			<MemoryRouter initialEntries={["/add/12345"]}>
+				<Router />
+			</MemoryRouter>
+		);
+		expect(wrapper.find(ListPage)).toHaveLength(0);
+		expect(wrapper.find(AddPage)).toHaveLength(1);
+		expect(wrapper.find(EditPage)).toHaveLength(0);
+		expect(wrapper.find(NotFound)).toHaveLength(0);
+
+		// EditPage
+		wrapper = mount(
+			<MemoryRouter initialEntries={["/edit/12345"]}>
+				<Router />
+			</MemoryRouter>
+		);
+		expect(wrapper.find(ListPage)).toHaveLength(0);
+		expect(wrapper.find(AddPage)).toHaveLength(0);
+		expect(wrapper.find(EditPage)).toHaveLength(1);
 		expect(wrapper.find(NotFound)).toHaveLength(0);
 	});
 });
